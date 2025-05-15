981dacb4086ef8839a82613bafdac0ef51a64635
Camille Onoda <info@camilleonoda.com>
2025-05-15 15:26:19 +0900

    create a props_to_html() function that return a string representing the attributes of an HTML node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,3 +9,24 @@ class HTMLNode():
 
     def to_html(self):
         raise NotImplementedError
+    
+    def props_to_html(self):
+        """Return a string that represents the HTML attributes of the node.
+        
+        If self.props:
+        
+        {
+            "href": "website link",
+            "target": "_blank",
+        }
+        
+        Then self.props_to_html() should return:
+         href="website link" target="_blank"
+
+        Notice the leading space character before href and before target.
+        """
+
+        string_attributes = ""
+        for key, value in self.props.items():
+            string_attributes += f" {key}" + "=" + f'"{value}"'
+        return string_attributes
