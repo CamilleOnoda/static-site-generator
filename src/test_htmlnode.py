c910b778b85b566e2c3fa5d15b89874b4f1d48c2
Camille Onoda <info@camilleonoda.com>
2025-05-20 13:50:02 +0900

    create a test to ensure that the value is returned if a LeafNode does not have any tag. Create a test to ensure the correct representation of a LeafNode object.

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -9,11 +9,14 @@ class TestHTMLNode(unittest.TestCase):
             }
         node = HTMLNode(props=attr_dict)
         str_repr = ' href="website link" target="_blank"'
+        node1 = HTMLNode(props=None)
+        str_repr1 = ""
+        self.assertEqual(node1.props_to_html(), str_repr1)
         self.assertEqual(node.props_to_html(), str_repr)
 
     def test_htmlNode_object(self):
         node = HTMLNode("h1","hello world")
-        self.assertEqual(node.__repr__(),"HTMLNode(h1, hello world, None, None)")
+        self.assertEqual(node.__repr__(),"HTMLNode(h1, hello world, children: None, None)")
 
     def test_default_to_none(self):
         node = HTMLNode("h1","hello world",)
@@ -21,5 +24,16 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
-        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        self.assertEqual(node.to_html(),"<p>Hello, world!</p>")
         
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(),'<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "I don't have any tag")
+        self.assertEqual(node.to_html(), "I don't have any tag")
+
+    def test_leafNode_object(self):
+        node = LeafNode("p", "hello world")
+        self.assertEqual(node.__repr__(), "LeafNode(p, hello world, None)")
