7decdaba3ed7b0241348208ec52e0d00d5db0eec
Camille Onoda <info@camilleonoda.com>
2025-05-20 14:15:09 +0900

    Create the ParentNode subclass

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -45,6 +45,15 @@ class LeafNode(HTMLNode):
     
     def __repr__(self):
         return f'LeafNode({self.tag}, {self.value}, {self.props})'
+    
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, children=children, props=props)
+
+
+
+
         
        
 
