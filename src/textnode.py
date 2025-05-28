61b1086e79117e0e1b65a1360fa18dfc064c695a
Camille Onoda <info@camilleonoda.com>
2025-05-28 16:32:55 +0900

    create a function to convert a TextNode to an HTMLNode (specifically a LeafNode)

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -9,6 +10,8 @@ class TextType(Enum):
     IMAGE = "image"
 
 
+# TextNode class represents various types of inline text that can exist
+# in HTML and markdown
 class TextNode():
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -28,3 +31,22 @@ class TextNode():
         
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type.value}, {self.url})'
+    
+    
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text) 
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", text_node.text, {"src": text_node.url,
+                                                    "alt": text_node.text})
+        case _:
+            raise Exception(f"text_type must be a TextType enum instance: {text_node.text_type}")
