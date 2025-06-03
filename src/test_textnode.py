42393e55924967136ee9f5f0d8adb7d16a6b6332
Camille Onoda <info@camilleonoda.com>
2025-06-03 14:56:07 +0900

    Create a TestMarkdownToTextNode class
    - test with bold delimiter
    - test with italic delimiter
    - test with code delimiter
    - test for invalid markdown (Exception raised)

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
-from textnode import TextNode, TextType
-from textnode import text_node_to_html_node
+from textnode import TextNode, TextType, text_node_to_html_node
+from markdown import split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -118,6 +118,40 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         self.assertEqual(html_node.value, "")
         self.assertEqual(html_node.props, {"src": "https://www.boot.dev",
                                            "alt": ""})
+
+
+class TestMarkdownToTextNode(unittest.TestCase):
+    def test_markdown_delimiter_bold(self):
+        node = TextNode("This is a text with a **word** in bold", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [
+            TextNode("This is a text with a ", TextType.TEXT),
+            TextNode("word", TextType.BOLD),
+            TextNode(" in bold", TextType.TEXT)]
+            )
+        
+    def test_markdown_delimiter_italic(self):
+        node = TextNode("This is a text with a _word_ in italic", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(new_nodes, [
+            TextNode("This is a text with a ", TextType.TEXT),
+            TextNode("word", TextType.ITALIC),
+            TextNode(" in italic", TextType.TEXT)]
+            )
+                
+    def test_markdown_delimiter_code(self):
+        node = TextNode("This is a text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+            TextNode("This is a text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT)]
+            )
+        
+    def test_invalid_Markdown(self):
+        with self.assertRaises(Exception):
+            node = TextNode("This is a text with a `code block word", TextType.TEXT)
+            new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
 
 
 if __name__ == "__main__":
