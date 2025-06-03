72a2f8442828911141b0703efb2882003e639174
Camille Onoda <info@camilleonoda.com>
2025-06-03 15:07:41 +0900

    Test for a different markdown syntax for bold and italic

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -121,7 +121,7 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
 
 
 class TestMarkdownToTextNode(unittest.TestCase):
-    def test_markdown_delimiter_bold(self):
+    def test_markdown_delimiter_bold_two_asterisks(self):
         node = TextNode("This is a text with a **word** in bold", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
         self.assertEqual(new_nodes, [
@@ -130,9 +130,27 @@ class TestMarkdownToTextNode(unittest.TestCase):
             TextNode(" in bold", TextType.TEXT)]
             )
         
-    def test_markdown_delimiter_italic(self):
+    def test_markdown_delimiter_bold_two_underscores(self):
+        node = TextNode("This is a text with a __word__ in bold", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "__", TextType.BOLD)
+        self.assertEqual(new_nodes, [
+            TextNode("This is a text with a ", TextType.TEXT),
+            TextNode("word", TextType.BOLD),
+            TextNode(" in bold", TextType.TEXT)]
+            )
+        
+    def test_markdown_delimiter_italic_underscore(self):
         node = TextNode("This is a text with a _word_ in italic", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(new_nodes, [
+            TextNode("This is a text with a ", TextType.TEXT),
+            TextNode("word", TextType.ITALIC),
+            TextNode(" in italic", TextType.TEXT)]
+            )
+        
+    def test_markdown_delimiter_italic_asterisk(self):
+        node = TextNode("This is a text with a *word* in italic", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "*", TextType.ITALIC)
         self.assertEqual(new_nodes, [
             TextNode("This is a text with a ", TextType.TEXT),
             TextNode("word", TextType.ITALIC),
