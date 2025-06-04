01e84dfaf20e8d84bc7c7023dcff573adb53b790
Camille Onoda <info@camilleonoda.com>
2025-06-04 12:53:44 +0900

    Create two functions to extract images and links with regex - images: returns a tuple that contains the alt text and the URL of any markdown images - links: returns a tuple that contains the anchor text and the URL

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -7,42 +8,45 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     It should return a new list of nodes, where any "text" type nodes 
     in the input list are (potentially) split into multiple nodes 
     based on the syntax. 
-
-    --For example:
-    
-    node = TextNode("This is text with a `code block` word", TextType.TEXT)
-    
-    new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-    
-    --new_nodes becomes:
-    
-    [
-        TextNode("This is text with a ", TextType.TEXT),
-        TextNode("code block", TextType.CODE),
-        TextNode(" word", TextType.TEXT),
-    ]
     """
     new_nodes = []
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
         else:
-            split_old_node = (old_node.text).split(delimiter)
-            if len(split_old_node) % 2 == 0:
-                raise Exception(
+            sections = (old_node.text).split(delimiter)
+            if len(sections) % 2 == 0:
+                raise ValueError(
                     "Invalid Markdown syntax: " \
                     "check the opening and closing delimiters"
                     )
             else:
-                for idx in range(len(split_old_node)):
-                    if split_old_node[idx] == "":
+                for idx in range(len(sections)):
+                    if sections[idx] == "":
                         continue
                     if idx % 2 == 0:
-                        new_nodes.append(TextNode(split_old_node[idx], 
+                        new_nodes.append(TextNode(sections[idx], 
                                                   TextType.TEXT))
                     else:
-                        new_nodes.append(TextNode(split_old_node[idx], 
+                        new_nodes.append(TextNode(sections[idx], 
                                                   text_type))
 
     return new_nodes
+
+
+def extract_markdown_images(text):
+    """Takes raw markdown text and returns a list of tuples. 
+    
+    Each tuple should contain the alt text and the URL of any markdown images.
+    
+    """
+    matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+    return matches
+
+
+def extract_markdown_links(text):
+    """Extracts markdown links.
+    It should return tuples of anchor text and URLs."""
+    matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
+    return matches
 
