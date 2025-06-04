66de8e20571784115b1f15219a3de9af84d9bbbb
Camille Onoda <info@camilleonoda.com>
2025-06-04 10:18:13 +0900

    Remove the tests for inline markdown

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType, text_node_to_html_node
-from markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -118,58 +118,6 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         self.assertEqual(html_node.value, "")
         self.assertEqual(html_node.props, {"src": "https://www.boot.dev",
                                            "alt": ""})
-
-
-class TestMarkdownToTextNode(unittest.TestCase):
-    def test_markdown_delimiter_bold_two_asterisks(self):
-        node = TextNode("This is a text with a **word** in bold", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-        self.assertEqual(new_nodes, [
-            TextNode("This is a text with a ", TextType.TEXT),
-            TextNode("word", TextType.BOLD),
-            TextNode(" in bold", TextType.TEXT)]
-            )
-        
-    def test_markdown_delimiter_bold_two_underscores(self):
-        node = TextNode("This is a text with a __word__ in bold", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "__", TextType.BOLD)
-        self.assertEqual(new_nodes, [
-            TextNode("This is a text with a ", TextType.TEXT),
-            TextNode("word", TextType.BOLD),
-            TextNode(" in bold", TextType.TEXT)]
-            )
-        
-    def test_markdown_delimiter_italic_underscore(self):
-        node = TextNode("This is a text with a _word_ in italic", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
-        self.assertEqual(new_nodes, [
-            TextNode("This is a text with a ", TextType.TEXT),
-            TextNode("word", TextType.ITALIC),
-            TextNode(" in italic", TextType.TEXT)]
-            )
-        
-    def test_markdown_delimiter_italic_asterisk(self):
-        node = TextNode("This is a text with a *word* in italic", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "*", TextType.ITALIC)
-        self.assertEqual(new_nodes, [
-            TextNode("This is a text with a ", TextType.TEXT),
-            TextNode("word", TextType.ITALIC),
-            TextNode(" in italic", TextType.TEXT)]
-            )
-                
-    def test_markdown_delimiter_code(self):
-        node = TextNode("This is a text with a `code block` word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        self.assertEqual(new_nodes, [
-            TextNode("This is a text with a ", TextType.TEXT),
-            TextNode("code block", TextType.CODE),
-            TextNode(" word", TextType.TEXT)]
-            )
-        
-    def test_invalid_Markdown(self):
-        with self.assertRaises(Exception):
-            node = TextNode("This is a text with a `code block word", TextType.TEXT)
-            new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
 
 
 if __name__ == "__main__":
