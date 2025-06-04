fe65f5dd9cf9e41d4ae471816c6ec827116fcfec
Camille Onoda <info@camilleonoda.com>
2025-06-04 12:55:42 +0900

    Add tests to extract markdown images and markdown links

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter
-import textnode
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 
 class TestMarkdownToTextNode(unittest.TestCase):
@@ -91,9 +90,25 @@ class TestMarkdownToTextNode(unittest.TestCase):
             )
         
     def test_invalid_Markdown(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(ValueError):
             node = TextNode("This is a text with a `missing delimiter", TextType.TEXT)
             new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+
+
+class TestExtractMarkdownLinksAndImages(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")],
+                              matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) "
+            "and [to youtube](https://www.youtube.com/@bootdotdev)")
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), 
+                              ("to youtube", "https://www.youtube.com/@bootdotdev")],
+                                matches)
 
 
 if __name__ == "__main__":
