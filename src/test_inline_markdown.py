d5b9ed0a39fef50a556f89d6384d2fef65b36f1f
Camille Onoda <info@camilleonoda.com>
2025-06-09 12:57:43 +0900

    Create test_split_images() and test_split_links()

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,10 @@
 import unittest
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline_markdown import (split_nodes_delimiter, 
+                            extract_markdown_images,
+                            extract_markdown_links,
+                            split_nodes_link,
+                            split_nodes_image)
 
 
 class TestMarkdownToTextNode(unittest.TestCase):
@@ -94,6 +98,24 @@ class TestMarkdownToTextNode(unittest.TestCase):
             node = TextNode("This is a text with a `missing delimiter", TextType.TEXT)
             new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
 
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode(
+                    "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+                ),
+            ],
+            new_nodes,
+        )
+
 
 class TestExtractMarkdownLinksAndImages(unittest.TestCase):
     def test_extract_images(self):
@@ -102,7 +124,7 @@ class TestExtractMarkdownLinksAndImages(unittest.TestCase):
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")],
                               matches)
         
-    def test_images_extra_characters_excluded(self):
+    def test_extract_images_extra_characters_excluded(self):
         matches = extract_markdown_images(
             "This is a text with an ![[image] (https://i.imgur.com/zjjcJKZ.png)")
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
@@ -115,10 +137,49 @@ class TestExtractMarkdownLinksAndImages(unittest.TestCase):
                               ("to youtube", "https://www.youtube.com/@bootdotdev")],
                                 matches)
 
-    def test_links_extra_characters_excluded(self):
+    def test_extract_links_extra_characters_excluded(self):
         matches = extract_markdown_links(
             "This is a text with a link [[to boot dev](https://www.boot.dev)")
         self.assertListEqual([("to boot dev", "https://www.boot.dev")], matches)
+
+
+    def test_split_images(self):
+        node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) "
+        "and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+            TextNode("This is text with an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode(
+                "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                    )
+            ],
+            new_nodes
+        )
+
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) "
+            "and [to youtube](https://www.youtube.com/@bootdotdev)", 
+            TextType.TEXT
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+                         )
+            ],
+            new_nodes
+        )
 
 
 if __name__ == "__main__":
