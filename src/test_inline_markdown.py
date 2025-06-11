7d9d3d1ef3e9f23e431ff6074fe378df92f36d3a
Camille Onoda <info@camilleonoda.com>
2025-06-11 11:14:59 +0900

    Create test_split_nodes_link_invalidMarkdown() and test_text_to_textnodes() - check for invalid Markdown link syntax - should raise a ValueError - check the proper convertion of a raw string of markdown text into a list of TextNode objects

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,11 +1,11 @@
 import unittest
-from test_textnode import TestTextNode
 from textnode import TextNode, TextType
 from inline_markdown import (split_nodes_delimiter, 
                             extract_markdown_images,
                             extract_markdown_links,
                             split_nodes_link,
-                            split_nodes_image)
+                            split_nodes_image,
+                            text_to_textnodes)
 
 
 class TestMarkdownToTextNode(unittest.TestCase):
@@ -119,25 +119,29 @@ class TestMarkdownToTextNode(unittest.TestCase):
             new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
 
 
-    def test_split_links(self):
-        node = TextNode(
-            "This is text with a link [to boot dev](https://www.boot.dev) "
-            "and [to youtube](https://www.youtube.com/@bootdotdev)",
-            TextType.TEXT,
+    def test_text_to_textnodes(self):
+        node = TextNode("This is **text** with an _italic_ word and " \
+        "a `code block` and an " \
+        "![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) " \
+        "and a [link](https://boot.dev)",
+        TextType.TEXT
         )
-        new_nodes = split_nodes_link([node])
+        new_nodes = text_to_textnodes([node])
         self.assertListEqual(
             [
-                TextNode("This is text with a link ", TextType.TEXT),
-                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
-                TextNode(" and ", TextType.TEXT),
-                TextNode(
-                    "to youtube", TextType.LINK, 
-                    "https://www.youtube.com/@bootdotdev"
-                ),
-            ],
-            new_nodes,
-        )
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+                ], 
+                new_nodes
+                )
 
 
 class TestExtractMarkdownLinksAndImages(unittest.TestCase):
@@ -337,6 +341,15 @@ class TestExtractMarkdownLinksAndImages(unittest.TestCase):
             ],
             new_nodes
         )
+
+
+    def test_split_nodes_link_invalidMarkdown(self):
+        with self.assertRaises(ValueError):
+            node = TextNode(
+                "This is text with a [link to boot dev]https://www.boot.dev)", 
+                TextType.TEXT
+            )
+            new_nodes = split_nodes_link([node])
 
 
 if __name__ == "__main__":
