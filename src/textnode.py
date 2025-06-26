36fe29a1588d6a861b81b30567037e2691914520
Camille Onoda <info@camilleonoda.com>
2025-06-26 13:21:17 +0900

    Replace the value by an empty string for TextType.IMAGE

--- src/textnode.py
+++ src/textnode.py
@@ -46,7 +46,7 @@ def text_node_to_html_node(text_node):
         case TextType.LINK:
             return LeafNode("a", text_node.text, {"href": text_node.url})
         case TextType.IMAGE:
-            return LeafNode("img", text_node.text, {"src": text_node.url,
+            return LeafNode("img", "", {"src": text_node.url,
                                                     "alt": text_node.text})
         case _:
             raise Exception(f"text_type must be a TextType enum instance: {text_node.text_type}")
