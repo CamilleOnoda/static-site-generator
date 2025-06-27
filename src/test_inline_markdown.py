5fba284a1f6c571ce55ac754cb5f498ff25c1999
Camille Onoda <info@camilleonoda.com>
2025-06-27 12:02:16 +0900

    Fix test_markdown_to_html_node_heading() and add a bold markdown syntax

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -449,18 +449,23 @@ the **same** even with inline stuff
 
     def test_markdown_to_html_node_heading(self):
         md = """
-# Heading level 1
+# Heading **level** 1
+
 ## Heading level 2
+
 ### Heading level 3
+
 #### Heading level 4
+
 ##### Heading level 5
+
 ###### Heading level 6
 """
         node = markdown_to_html_node(md)
         html = node.to_html()
         self.assertEqual(
             html,
-            "<div><h1>Heading level 1</h1><h2>Heading level 2</h2><h3>Heading level 3</h3><h4>Heading level 4</h4><h5>Heading level 5</h5><h6>Heading level 6</h6></div>"
+            "<div><h1>Heading <b>level</b> 1</h1><h2>Heading level 2</h2><h3>Heading level 3</h3><h4>Heading level 4</h4><h5>Heading level 5</h5><h6>Heading level 6</h6></div>"
         )
 
 
