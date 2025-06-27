88370fc4dcb0da88d21bd08fba601162d60ae56a
Camille Onoda <info@camilleonoda.com>
2025-06-27 11:06:53 +0900

    Remove tests for the second type of italic and bold mardown syntax

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -13,21 +13,10 @@ from inline_markdown import (BlockType,
 
 
 class TestMarkdownToTextNode(unittest.TestCase):
-    def test_markdown_delimiter_bold_two_asterisks(self):
+    def test_markdown_delimiter_bold(self):
         node = TextNode("This is a text with a **word** in bold",
                         TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-        self.assertListEqual(new_nodes, [
-            TextNode("This is a text with a ", TextType.TEXT),
-            TextNode("word", TextType.BOLD),
-            TextNode(" in bold", TextType.TEXT)]
-            )
-        
-
-    def test_markdown_delimiter_bold_two_underscores(self):
-        node = TextNode("This is a text with a __word__ in bold",
-                        TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "__", TextType.BOLD)
         self.assertListEqual(new_nodes, [
             TextNode("This is a text with a ", TextType.TEXT),
             TextNode("word", TextType.BOLD),
@@ -47,33 +36,10 @@ class TestMarkdownToTextNode(unittest.TestCase):
             )
 
 
-    def test_markdown_delimiter_bold_underscores_multiple(self):
-        node = TextNode("This is a text with a __bold word__ and __another__",
-                        TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "__", TextType.BOLD)
-        self.assertListEqual(new_nodes, [
-            TextNode("This is a text with a ", TextType.TEXT),
-            TextNode("bold word", TextType.BOLD),
-            TextNode(" and ", TextType.TEXT),
-            TextNode("another", TextType.BOLD)]
-            )
-
-
-    def test_markdown_delimiter_italic_underscore(self):
+    def test_markdown_delimiter_italic(self):
         node = TextNode("This is a text with a _word_ in italic",
                         TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
-        self.assertListEqual(new_nodes, [
-            TextNode("This is a text with a ", TextType.TEXT),
-            TextNode("word", TextType.ITALIC),
-            TextNode(" in italic", TextType.TEXT)]
-            )
-        
-
-    def test_markdown_delimiter_italic_asterisk(self):
-        node = TextNode("This is a text with a *word* in italic",
-                        TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "*", TextType.ITALIC)
         self.assertListEqual(new_nodes, [
             TextNode("This is a text with a ", TextType.TEXT),
             TextNode("word", TextType.ITALIC),
@@ -85,18 +51,6 @@ class TestMarkdownToTextNode(unittest.TestCase):
         node = TextNode("This is a text with an _italic word_ and _another_",
                         TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
-        self.assertListEqual(new_nodes, [
-            TextNode("This is a text with an ", TextType.TEXT),
-            TextNode("italic word", TextType.ITALIC),
-            TextNode(" and ", TextType.TEXT),
-            TextNode("another", TextType.ITALIC)]
-            )
-                
-
-    def test_markdown_delimiter_italic_asterisk_multiple(self):
-        node = TextNode("This is a text with an *italic word* and *another*",
-                        TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "*", TextType.ITALIC)
         self.assertListEqual(new_nodes, [
             TextNode("This is a text with an ", TextType.TEXT),
             TextNode("italic word", TextType.ITALIC),
